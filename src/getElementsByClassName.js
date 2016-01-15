527b810c8356cdbd60babd8a7a13268a5b4ed815
Kathleen L <kathleeng.lopez@gmail.com>
2016-01-14 19:23:23 -0800

    Fix indentation

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,31 +7,31 @@
 
 
 var getElementsByClassName = function(className, node){
- 	//create an empty array to store the nodes with the matched classNames
-	var results = [];
+  //create an empty array to store the nodes with the matched classNames
+  var results = [];
 
-	//if a currentNode to start at is not passed in, then use the top of the HTML document
-	node = node || document.body;
+  //if a currentNode to start at is not passed in, then use the top of the HTML document
+  node = node || document.body;
 
-  	//some nodes have more than one class
-  	//need to break them up in order to compare the currentNode's className and the className you are looking for
-	var nodes = node.className.split(' ');
+  //some nodes have more than one class
+  //need to break them up in order to compare the currentNode's className and the className you are looking for
+  var nodes = node.className.split(' ');
 	
-  	//check the current node's className against the passed className
-	if(nodes.indexOf(className) >= 0) { //will return the index if matched (positive 	number or 0)
-    	//if the className matches, store that node in the results container
-		results.push(node);
-	}
+  //check the current node's className against the passed className
+  if(nodes.indexOf(className) >= 0) { //will return the index if matched (positive 	number or 0)
+    //if the className matches, store that node in the results container
+	results.push(node);
+  }
 	
 
-	//loop through the child nodes
-	for (var i = 0; i < node.children.length; i++) {
-		//for each child element, invoke getElementsByClassName
-		var childNodeResults = getElementsByClassName(className, node.children[i]);
-		//combine the results from parent and child nodes
-		results = results.concat(childNodeResults); 
-	}
+  //loop through the child nodes
+  for (var i = 0; i < node.children.length; i++) {
+	//for each child element, invoke getElementsByClassName
+	var childNodeResults = getElementsByClassName(className, node.children[i]);
+	//combine the results from parent and child nodes
+	results = results.concat(childNodeResults); 
+  }
 
-	//return the list of nodes with matching classNames
-	return results;
+  //return the list of nodes with matching classNames
+  return results;
 };
