a4dc49fb495451867ff6b3a69579de848cf9ab05
Kathleen L <kathleeng.lopez@gmail.com>
2016-01-14 18:52:39 -0800

    added additional parameter

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+
+var getElementsByClassName = function(className, currentNode){
   // your code here
+
+  //if a currentNode to start at is not passed in, then use the top of the HTML document
+  var node = node || document.body;
+  
+  //check the current node's className against the passed className
+
+
 };
