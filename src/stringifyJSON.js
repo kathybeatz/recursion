a84b5a98484d5e00fe1f168146fe9aaf912682a8
Kathleen L <kathleeng.lopez@gmail.com>
2016-01-14 17:58:13 -0800

    First spec (number) working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,19 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+//function should take in an object(number, boolean, string .. essentially everything is an object)
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  //Spec: 9
+  //check whether the object is a number
+  if(typeof obj === 'number'){
+  	//if it is, stringify it
+  	return obj.toString();
+  }
+
+
+
+
 };
