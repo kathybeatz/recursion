b86bf81d494bac9d8f41de96d9e426d96b47ca76
Kathleen L <kathleeng.lopez@gmail.com>
2016-01-14 19:21:19 -0800

    work on branches (child nodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,22 +5,33 @@
 
 // But instead we're going to implement it from scratch:
 
-var getElementsByClassName = function(className, currentNode){
-  // your code here
 
-  //create an empty array to store the nodes with the matched classNames
-  var results = [];
+var getElementsByClassName = function(className, node){
+ 	//create an empty array to store the nodes with the matched classNames
+	var results = [];
 
-  //if a currentNode to start at is not passed in, then use the top of the HTML document
-  var node = node || document.body;
+	//if a currentNode to start at is not passed in, then use the top of the HTML document
+	node = node || document.body;
 
-  //some nodes have more than one class
-  //need to break them up in order to compare the currentNode's className and the className you are looking for
-  var splitClasses = node.className.split('');
-  //check the current node's className against the passed className
-  if(splitClasses.indexOf( className ) >= 0){ //will return the index if matched (positive number or 0)
-    //if the className matches, store that node in the results container
-    results.push(currentNode);
-  }
+  	//some nodes have more than one class
+  	//need to break them up in order to compare the currentNode's className and the className you are looking for
+	var nodes = node.className.split(' ');
+	
+  	//check the current node's className against the passed className
+	if(nodes.indexOf(className) >= 0) { //will return the index if matched (positive 	number or 0)
+    	//if the className matches, store that node in the results container
+		results.push(node);
+	}
+	
 
+	//loop through the child nodes
+	for (var i = 0; i < node.children.length; i++) {
+		//for each child element, invoke getElementsByClassName
+		var childNodeResults = getElementsByClassName(className, node.children[i]);
+		//combine the results from parent and child nodes
+		results = results.concat(childNodeResults); 
+	}
+
+	//return the list of nodes with matching classNames
+	return results;
 };
